9e1d2945a041b7d7aecf9c2d15757ec99f4e07fb
Dylan Fareed <email@dylanfareed.com>
2024-10-21 21:59:01 -0400

    feat: polish in the footer + tracking README usage section

--- lib/components/Timeline/SecondaryStep.tsx
+++ lib/components/Timeline/SecondaryStep.tsx
@@ -12,7 +12,7 @@ export const SecondaryStep = ({ config }: { config: TrackingConfig }) => {
       >
         <path
           d="M8 4C8 6.20914 6.20914 8 4 8C1.79086 8 0 6.20914 0 4C0 1.79086 1.79086 0 4 0C6.20914 0 8 1.79086 8 4Z"
-          fill="#D7D6D0"
+          fill={config.style.color.iconSecondary}
         />
       </svg>
     </div>
