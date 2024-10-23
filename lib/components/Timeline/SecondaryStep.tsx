d30936e9ec932714afcf7f4e0d5140ea899f060d
Dylan Fareed <email@dylanfareed.com>
2024-10-22 23:45:42 -0400

    fix: secondary timeline steps color presentation for minimal layouts (#46)

--- lib/components/Timeline/SecondaryStep.tsx
+++ lib/components/Timeline/SecondaryStep.tsx
@@ -12,7 +12,7 @@ export const SecondaryStep = ({ config }: { config: TrackingConfig }) => {
       >
         <path
           d="M8 4C8 6.20914 6.20914 8 4 8C1.79086 8 0 6.20914 0 4C0 1.79086 1.79086 0 4 0C6.20914 0 8 1.79086 8 4Z"
-          fill={config.style.color.iconSecondary}
+          fill={config.style.color.iconTertiary}
         />
       </svg>
     </div>
