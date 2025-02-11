8c5c1828a4413c44ec3bc4e27c2f7ec608c3b843
Otavio Jacobi <otaviojacobi@gmail.com>
2025-02-11 14:07:17 -0300

    feat: expose Estimate, Tracking and MetadataTypes typings

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,8 @@
 import Arta from './arta';
 import './components/Modal/index.css';
 import './components/TrackingDrawer/index.css';
+import Estimate from './estimate';
+import Tracking from './tracking';
 
 const init = () => {
   (<any>window).Arta = new Arta();
@@ -11,6 +13,11 @@ init();
 export * from './arta';
 export * from './estimate';
 export * from './tracking';
+export {
+  Estimate,
+  Tracking
+};
+export * from './MetadataTypes';
 export type {
   EstimateConfig,
   EstimateBody,
